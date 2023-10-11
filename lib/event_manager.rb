8d8f31fc729d80836cf17b88b32d9a450114b63c
Njonge9 <frednjonge9@gmail.com>
2023-10-11 14:29:09 +0300

    Skipping the header row

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -40,10 +40,16 @@ contents = File.read('event_attendees.csv')
 # end
 
 # Third method
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(',')
-  name = columns[2]
+# lines = File.readlines('event_attendees.csv')
+# lines.each_with_index do |line, index|
+#   next if index == 0
+#   columns = line.split(',')
+#   name = columns[2]
+#   puts name
+# end
+
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
