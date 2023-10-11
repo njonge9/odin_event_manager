b6f9318b9b5f0a93695417ec14a993d9c962687e
Njonge9 <frednjonge9@gmail.com>
2023-10-11 15:07:31 +0300

    cleaning up our zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -97,8 +97,10 @@ contents.each do |row|
   full_name = "#{first_name} #{last_name}"
   zipcode = row[:zipcode]
   # p zipcode.length
-
-  if zipcode.length < 5
+  # Handle missing zip codes
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
     zipcode = zipcode.rjust(5,'0')
   elsif zipcode.length > 5
     zipcode = zipcode[0..4]
