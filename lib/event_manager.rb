1e3fa1b374e40af679a89037cf2f146f15a573cd
Njonge9 <frednjonge9@gmail.com>
2023-10-11 15:16:45 +0300

    Refactoring clean zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -87,15 +87,16 @@ contents = File.read('event_attendees.csv')
 # Moving clean zipcodes to a method
 def clean_zipcode(zipcode)
    # Handle missing zip codes
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5,'0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
-  end
+  # if zipcode.nil?
+  #   zipcode = '00000'
+  # elsif zipcode.length < 5
+  #   zipcode = zipcode.rjust(5,'0')
+  # elsif zipcode.length > 5
+  #   zipcode = zipcode[0..4]
+  # else
+  #   zipcode
+  # end
+  zipcode.to_s.rjust(5,'0')[0..4]
 end
 
 # Cleaning up our zip codes
