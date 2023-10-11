2a04ed682ee4ae6075b52faed385db99cf3c8e74
Njonge9 <frednjonge9@gmail.com>
2023-10-11 12:39:38 +0300

    Read the file line by line

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,4 +3,10 @@ puts 'Event Manager Initialized!'
 # Read the file contents
 contents = File.read('event_attendees.csv')
 
-puts contents
+# puts contents
+
+# Read the file line by line
+lines = File.readlines('event_attendees.csv')
+lines.each do |line|
+  puts line
+end
